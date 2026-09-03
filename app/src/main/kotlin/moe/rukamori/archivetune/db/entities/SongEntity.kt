96cbe2f57b38916a29093c1e62994c5a287354d6
shinonatsukii <oneofremii@gmail.com>
2026-09-03 13:26:48 +0700

    refactor(SongEntity): remove YouTube side effects from entity
    Refs: #1259

--- app/src/main/kotlin/moe/rukamori/archivetune/db/entities/SongEntity.kt
+++ app/src/main/kotlin/moe/rukamori/archivetune/db/entities/SongEntity.kt
@@ -12,11 +12,6 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-import moe.rukamori.archivetune.innertube.YouTube
 import java.time.LocalDateTime
 
 @Immutable
@@ -68,12 +63,7 @@ data class SongEntity(
                 liked = !liked,
                 likedDate = if (!liked) LocalDateTime.now() else null,
                 inLibrary = if (!liked) inLibrary ?: LocalDateTime.now() else inLibrary,
-            ).also {
-                CoroutineScope(Dispatchers.IO).launch {
-                    YouTube.likeVideo(id, !liked)
-                    this.cancel()
-                }
-            }
+            )
         }
 
     fun toggleLibrary() =
@@ -83,3 +73,8 @@ data class SongEntity(
             likedDate = if (inLibrary == null) likedDate else null,
         )
 }
+
+data class LikedSongDate(
+    val id: String,
+    val likedDate: LocalDateTime?,
+)
